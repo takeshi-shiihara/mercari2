60123d688f1782d85cb90fa38d6c50ea6a235a06
takeshi-shiihara <shi4490@icloud.com>
2019-09-23 20:37:58 +0900

    修正(自動デプロイに関して)

--- config/deploy.rb
+++ config/deploy.rb
@@ -35,13 +35,40 @@ namespace :deploy do
 end
 
 set :default_env, {
-  rbenv_root: "/usr/local/rbenv",
-  path: "/usr/local/rbenv/shims:/usr/local/rbenv/bin:$PATH",
+  # rbenv_root: "/usr/local/rbenv",
+  # path: "/usr/local/rbenv/shims:/usr/local/rbenv/bin:$PATH",
   BASIC_AUTH_USER: ENV["BASIC_AUTH_USER"],
   BASIC_AUTH_PASSWORD: ENV["BASIC_AUTH_PASSWORD"]
   #AWS_ACCESS_KEY_ID: ENV["AWS_ACCESS_KEY_ID"],
   #AWS_SECRET_ACCESS_KEY: ENV["AWS_SECRET_ACCESS_KEY"]
 }
+
+# secrets.yml用のシンボリックリンクを追加
+set :linked_files, %w{ config/credentials.yml.enc }
+append :linked_files, 'config/master.key'
+
+# 元々記述されていた after 「'deploy:publishing', 'deploy:restart'」以下を削除して、次のように書き換え
+
+after 'deploy:publishing', 'deploy:restart'
+namespace :deploy do
+  task :restart do
+    invoke 'unicorn:restart'
+  end
+
+  desc 'upload credentials.yml.enc'
+  desc 'upload master.key'
+  task :upload do
+    on roles(:app) do |host|
+      if test "[ ! -d #{shared_path}/config ]"
+        execute "mkdir -p #{shared_path}/config"
+      end
+      upload!('config/credentials.yml.enc', "#{shared_path}/config/credentials.yml.enc")
+      upload!('config/master.key', "#{shared_path}/config/master.key")
+    end
+  end
+  before :starting, 'deploy:upload'
+  after :finishing, 'deploy:cleanup'
+end
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
