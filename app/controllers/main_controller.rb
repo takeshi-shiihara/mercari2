e754a36d564250015427d313b1b780aea6fa0ee0
Koki7749 <ito8864@gmail.com>
2019-09-25 14:49:59 +0900

    user registration

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,13 +1,25 @@
 class MainController < ApplicationController
 
-  before_action :move_to_index, except: :index #ログインしていない時は出品できない仕様#
+#  before_action :move_to_index, except: :index #ログインしていない時は出品できない仕様#
 
   def index
   end
 
-  private
-  def move_to_index  #ログインしていない時は出品できない仕様#
-    redirect_to action: :index unless user_signed_in?
+  def step2
   end
+
+  def step3
+  end
+
+  def step4
+  end
+
+  def step5
+  end
+
+  private
+#  def move_to_index  #ログインしていない時は出品できない仕様#
+#    redirect_to action: :index unless user_signed_in?
+#  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   root 'main#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get 'step2' => 'main#step2'
+  get 'step3' => 'main#step3'
+  get 'step4' => 'main#step4'
+  get 'step5' => 'main#step5'
+
+
 end
