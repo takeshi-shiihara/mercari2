98fb547b65dfa1bd8a20f7c0ccfa0422e378a301
Koki7749 <ito8864@gmail.com>
2019-09-27 21:05:31 +0900

    create login function

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,10 +4,16 @@ class ApplicationController < ActionController::Base
   before_action :basic_auth, if: :production?
   protect_from_forgery with: :exception
 
+
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :first_name, :last_name , :first_name_kana, :last_name_kana])
-  # 謝って一般の人がアクセスしないようにします
+  devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :first_name, :last_name , :first_name_kana, :last_name_kana])
+  #謝って一般の人がアクセスしないようにします
   end
+
+#  def after_sign_in_path_for(resource)
+#    step2_path(resource)
+#  end
+
 
   private
 
@@ -20,5 +26,5 @@ class ApplicationController < ActionController::Base
       username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
     end
   end
-  
+
 end
--- db/migrate/20190922062344_devise_create_users.rb
+++ db/migrate/20190922062344_devise_create_users.rb
@@ -7,7 +7,6 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
       t.string :encrypted_password, null: false, default: ""
       t.string :nickname,           null: false, unique: :true
       t.string :email,              null: false
-      t.string :phone_number,       null: false, unique: :true
       t.string :first_name,         null: false
       t.string :last_name,          null: false
       t.string :first_name_kana,    null: false
