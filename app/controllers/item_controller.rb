459a010fbad5a5b15a844c786aeec1560c552e9d
takeshi-shiihara <shi4490@icloud.com>
2019-09-28 16:07:43 +0900

    商品詳細画面:修正！

--- app/controllers/item_controller.rb
+++ app/controllers/item_controller.rb
@@ -4,6 +4,7 @@ class ItemController < ApplicationController
   end
 
   def show
+    # @item = Item.find(params[:id])
   end
 
   def hop1
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
         :recoverable, :rememberable, :validatable
         validates :nickname, :first_name, :last_name, :first_name_kana, :last_name_kana, presence: true
+  
+  has_many :items
 end
