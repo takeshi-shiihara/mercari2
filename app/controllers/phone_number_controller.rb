24dfa4044043f32e80106be2c78a9ba1c240730c
Koki7749 <ito8864@gmail.com>
2019-09-28 12:48:55 +0900

    registarete phone_number

---
+++ app/controllers/phone_number_controller.rb
@@ -0,0 +1,23 @@
+class PhoneNumberController < ApplicationController
+
+  def new
+    @phone_number = PhoneNumber.new
+  end
+
+  def create
+    @phone_number = PhoneNumber.create(phone_number_params)
+    if @phone_number.save
+      redirect_to controller: :main, action: :step3
+    else
+    redirect_to controller: :main, action: :index
+    end
+  end
+
+
+private
+
+def phone_number_params
+  params.require(:phone_number).permit(:phone_number).merge(user_id:current_user.id)
+end
+
+end
