a869f8a447e1a3ee971d61ce756764cd8207da79
macken-hem <ken.kgaf@gmail.com>
2019-10-01 14:31:33 +0900

    Update item_controller.rb

--- app/controllers/item_controller.rb
+++ app/controllers/item_controller.rb
@@ -18,7 +18,7 @@ class ItemController < ApplicationController
   end
 
   def create
-    @item = Item.create(item_params)
+    @item = Item.new(item_params)
     if @item.save
       redirect_to controller: :main, action: :index
     else
