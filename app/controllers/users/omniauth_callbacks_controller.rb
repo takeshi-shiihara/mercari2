ca2f4a2c7f98cd86d0ee16de3049ca1f330ed02e
Koki7749 <ito8864@gmail.com>
2019-10-02 18:23:11 +0900

    edit

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -3,32 +3,23 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
   def facebook
-    # You need to implement the method below in your model (e.g. app/models/user.rb)
-    info = User.find_oauth(request.env["omniauth.auth"])
-    @user = info[:user]
-    if @user.persisted? #ユーザー情報が存在するならば(メルカリでFB登録済みのユーザーである場合)
-      sign_in_and_redirect @user, event: :authentication #this will throw if @user is not activated
-      set_flash_message(:notice, :success, kind: "Facebook".capitalize) if is_navigational_format?
-    else#初めてFBで登録する
-      render template: "users/registrations/new" #redirect_to だと更新してしまうのでrenderで
-    end
-  end
-
-  def failure
-    redirect_to root_path and return
+    callback_for(:facebook)
   end
 
   def google_oauth2
-    # You need to implement the method below in your model (e.g. app/models/user.rb)
+    callback_for(:google_oauth2)
+  end
+
+  def callback_for(provider)
     info = User.find_oauth(request.env["omniauth.auth"])
     @user = info[:user]
-    #binding.pry
     if @user.persisted? #ユーザー情報が存在するならば(メルカリでFB登録済みのユーザーである場合)
-      sign_in_and_redirect @user, event: :authentication #this will throw if @user is not activated
+      sign_in_and_redirect @user, event: :authentication
       set_flash_message(:notice, :success, kind: "Facebook".capitalize) if is_navigational_format?
     else#初めてFBで登録する
       render template: "users/registrations/new" #redirect_to だと更新してしまうのでrenderで
     end
+  end
   end
 
   def failure
@@ -36,4 +27,3 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   end
 
 
-end
