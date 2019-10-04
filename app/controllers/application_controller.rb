9486cbb224c7e8b7c4b2ab7366b774fbae30616c
Koki7749 <ito8864@gmail.com>
2019-10-04 21:03:22 +0900

    registration transition

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,19 +3,21 @@ class ApplicationController < ActionController::Base
   before_action :basic_auth, if: :production?
   protect_from_forgery with: :exception
 
-
   def configure_permitted_parameters
   devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :first_name, :last_name , :first_name_kana, :last_name_kana])
   end
 
-#  def after_sign_in_path_for(resource)
-#  '/phone_number/new'
-#  end
-
+  #もし電話番号が登録されていなかったら
   def after_sign_in_path_for(resource)
-    '/phone_number/new'
+    @user = current_user
+    @address = @user.address
+    if @address.present?
+      root_path
+    else
+      '/phone_number/new'
   end
-
+end
+end
   private
 
   def production?
@@ -25,6 +27,5 @@ class ApplicationController < ActionController::Base
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
       username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
-    end
   end
 end
